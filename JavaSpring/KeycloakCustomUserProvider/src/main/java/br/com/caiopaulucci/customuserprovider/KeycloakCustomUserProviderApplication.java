825d35c92f353428e62a7b1ed06d6760f6571632
Caio Paulucci <caio.paulucci@softplan.com.br>
2023-10-19 11:13:19 -0300

    Testes com SPI

---
+++ JavaSpring/KeycloakCustomUserProvider/src/main/java/br/com/caiopaulucci/customuserprovider/KeycloakCustomUserProviderApplication.java
@@ -0,0 +1,13 @@
+package br.com.caiopaulucci.customuserprovider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeycloakCustomUserProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeycloakCustomUserProviderApplication.class, args);
+	}
+
+}
---
+++ JavaSpring/KeycloakCustomUserProvider/src/test/java/br/com/caiopaulucci/customuserprovider/KeycloakCustomUserProvider/KeycloakCustomUserProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.caiopaulucci.customuserprovider.KeycloakCustomUserProvider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeycloakCustomUserProviderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
